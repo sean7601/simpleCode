d13f6992a61240f22333c19b8e7038368c239adc
sean7601 <sean7601@gmail.com>
2022-12-23 12:30:54 -0500

    fixed the bug where you could reopen after edit

--- editFile.js
+++ editFile.js
@@ -3,4 +3,5 @@ var editFile = {}
 editFile.letsAddStuff = function(){
  console.log("ADD")
   whatever
+  tested2228989nnntttyt11212p533
 }
--- editors/editor.js
+++ editors/editor.js
@@ -2,15 +2,16 @@ var editor = {}
 
 editor.instance = null
 editor.openFile = async function(uuid){
+
     //go over loadFolder.fileStructure array and find the file with the uuid
     let file = loadFolder.fileStructure.find(file => file.uuid == uuid)
+    console.log(file)
     file = file.contents
-    
-    let reader = new FileReader();
-    reader.onload = function(e) {
+    if(file.hasOwnProperty("text")){
         document.getElementById("editor").innerHTML = ""
+        console.log(file)
         editor.instance = CodeMirror(document.getElementById("editor"), {
-            value: e.target.result,
+            value: file.text,
             mode:  "javascript",
             lineNumbers: true,
             height: "auto",
@@ -22,12 +23,44 @@ editor.openFile = async function(uuid){
             console.log(file)
             let fileHandle = file.entry
             let contents = editor.instance.getValue()
+            file.text = contents
             fileHandle.createWritable().then(writable => {
                 writable.write(contents);
                 writable.close();
             })
+            //re read the file from loadFolder.fileHandle object and update the loadFolder.fileStructure object
+
+            
         })
     }
-    reader.readAsText(file);
-    
+    else{
+        
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            console.log(e)
+            console.log(e.target.result)
+            document.getElementById("editor").innerHTML = ""
+            file.text = e.target.result
+            editor.instance = CodeMirror(document.getElementById("editor"), {
+                value: e.target.result,
+                mode:  "javascript",
+                lineNumbers: true,
+                height: "auto",
+            });
+
+            editor.instance.on("change",function(){
+                //save the file
+                let file = loadFolder.fileStructure.find(file => file.uuid == uuid)
+                console.log(file)
+                let fileHandle = file.entry
+                let contents = editor.instance.getValue()
+                file.contents.text = contents
+                fileHandle.createWritable().then(writable => {
+                    writable.write(contents);
+                    writable.close();
+                })
+            })
+        }
+        reader.readAsText(file);
+    }
 }
